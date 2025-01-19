d4feeb5c1d61149160f2c33587d325610ec7fb9b
Ashish Singh <ashishsinghrana39@gmail.com>
2025-01-19 11:15:05 +0530

    committed

---
+++ Power_Calculation.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Power_Calculation{
+   public static void main(String[] args){
+         Scanner sc=new Scanner(System.in);
+		 double base=sc.nextInt();
+		 double exponent=sc.nextInt();
+		 
+		 double power=Math.pow(base,exponent);
+		 System.out.println(power);
+   }
+}
