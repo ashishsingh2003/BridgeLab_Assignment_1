6035ca2f7681d48e8d67900d7f8a4fb792b47b9c
Ashish Singh <ashishsinghrana39@gmail.com>
2025-01-19 11:30:22 +0530

    committed

---
+++ Average_number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Average_number{
+   public static void main(String[] args){
+         Scanner sc=new Scanner(System.in);
+		 double a=sc.nextInt();
+		 double b=sc.nextInt();
+		 double c=sc.nextInt();
+		 double average=(a+b+c)/3;
+		 System.out.println(average);
+   }
+}
---
+++ Kilo_to_Miles.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Kilo_to_Miles{
+   public static void main(String[] args){
+         Scanner sc=new Scanner(System.in);
+		 double kilometer=sc.nextInt();
+		 double miles=kilometer*0.621371;
+		 System.out.println(miles);
+   }
+}
